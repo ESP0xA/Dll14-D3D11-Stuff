8e82e60cb0816482a926ce5df68cf52dc6296ad1
zhangxingyu <rpm@live.com>
2020-12-30 14:12:23 +0800

    Add project files.

---
+++ dllmain.cpp
@@ -0,0 +1,136 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include <Windows.h>
+#include <vector>
+#include <d3d11.h>
+#include <D3Dcompiler.h>//generateshader
+#pragma comment(lib, "D3dcompiler.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "winmm.lib") //timeGetTime
+#include "main.h"
+#include "Minhook/include/MinHook.h"
+#include "rD3D11.h"
+#pragma warning( disable : 4244 )
+
+typedef HRESULT(__stdcall* D3D11PresentHook) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
+D3D11PresentHook phookD3D11Present = NULL;
+
+ID3D11Device* pDevice = NULL;
+ID3D11DeviceContext* pContext = NULL;
+
+DWORD_PTR* pSwapChainVtable = NULL;
+DWORD_PTR* pContextVTable = NULL;
+DWORD_PTR* pDeviceVTable = NULL;
+
+
+rD3D11 rd11;
+HRESULT __stdcall hookD3D11Present(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
+{
+	//if (!rd11.pDevice || rd11.pSwapchain != pThis)
+	//{
+	//}
+	rd11.InitD3DDraw(pSwapChain);
+	//enable this to test or debug viewport
+	rd11.TestRender();
+	return phookD3D11Present(pSwapChain, SyncInterval, Flags);
+}
+
+
+const int MultisampleCount = 1; // Set to 1 to disable multisampling
+LRESULT CALLBACK DXGIMsgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) { return DefWindowProc(hwnd, uMsg, wParam, lParam); }
+DWORD __stdcall InitializeHook(LPVOID)
+{
+	IDXGISwapChain* pSwapChain;
+
+	// creats window.
+	WNDCLASSEXA wc = { sizeof(WNDCLASSEX), CS_CLASSDC, DXGIMsgProc, 0L, 0L, GetModuleHandleA(NULL), NULL, NULL, NULL, NULL, "WX", NULL };
+	RegisterClassExA(&wc);
+	HWND hWnd = CreateWindowA("WX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, NULL, NULL, wc.hInstance, NULL);
+
+	// 
+	D3D_FEATURE_LEVEL requestedLevels[] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_1 };
+	D3D_FEATURE_LEVEL obtainedLevel;
+	ID3D11Device* d3dDevice = nullptr;
+	ID3D11DeviceContext* d3dContext = nullptr;
+
+	// Describes a swap chain.
+	DXGI_SWAP_CHAIN_DESC scd;
+	SecureZeroMemory(&scd, sizeof(scd));
+	scd.BufferCount = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = MultisampleCount;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.Windowed = ((GetWindowLongPtr(hWnd, GWL_STYLE) & WS_POPUP) != 0) ? false : true;
+
+	// LibOVR 0.4.3 requires that the width and height for the backbuffer is set even if
+	// you use windowed mode, despite being optional according to the D3D11 documentation.
+	scd.BufferDesc.Width = 1;
+	scd.BufferDesc.Height = 1;
+	scd.BufferDesc.RefreshRate.Numerator = 0;
+	scd.BufferDesc.RefreshRate.Denominator = 1;
+
+	UINT createFlags = 0;
+#ifdef _DEBUG
+	// This flag gives you some quite wonderful debug text. Not wonderful for performance, though!
+	createFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	IDXGISwapChain* d3dSwapChain = 0;
+
+	if (FAILED(D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		createFlags,
+		requestedLevels,
+		sizeof(requestedLevels) / sizeof(D3D_FEATURE_LEVEL),
+		D3D11_SDK_VERSION,
+		&scd,
+		&pSwapChain,
+		&pDevice,
+		&obtainedLevel,
+		&pContext)))
+	{
+		MessageBox(hWnd, L"Failed to create directX device and swapchain!", L"Error", MB_ICONERROR);
+		return NULL;
+	}
+
+
+	pSwapChainVtable = (DWORD_PTR*)pSwapChain;
+	pSwapChainVtable = (DWORD_PTR*)pSwapChainVtable[0];
+
+	pContextVTable = (DWORD_PTR*)pContext;
+	pContextVTable = (DWORD_PTR*)pContextVTable[0];
+
+
+	pDeviceVTable = (DWORD_PTR*)pDevice;
+	pDeviceVTable = (DWORD_PTR*)pDeviceVTable[0];
+
+	if (MH_Initialize() != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)pSwapChainVtable[8], hookD3D11Present, reinterpret_cast<void**>(&phookD3D11Present)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)pSwapChainVtable[8]) != MH_OK) { return 1; }
+}
+
+BOOL __stdcall DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+		DisableThreadLibraryCalls(hModule);
+		GetModuleFileName(hModule, (LPWSTR)dlldir, 512);
+		for (size_t i = strlen(dlldir); i > 0; i--) { if (dlldir[i] == '\\') { dlldir[i + 1] = 0; break; } }
+		CreateThread(NULL, 0, InitializeHook, NULL, 0, NULL);
+		break;
+
+	case DLL_PROCESS_DETACH: // A process unloads the DLL.
+		if (MH_Uninitialize() != MH_OK) { return 1; }
+		if (MH_DisableHook((DWORD_PTR*)pSwapChainVtable[8]) != MH_OK) { return 1; }
+		break;
+	}
+	return TRUE;
+}
